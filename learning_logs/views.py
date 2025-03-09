3bd061a9177a5ae649ba2e51c810e8db01c68b71
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-03-09 15:28:01 -0600

    Added Context values for topic view and template.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
@@ -6,5 +7,8 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 def topics(request):
-    """The home page for learning log."""
-    return render(request, 'learning_logs/topics.html')
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+
+    return render(request, 'learning_logs/topics.html',context)
